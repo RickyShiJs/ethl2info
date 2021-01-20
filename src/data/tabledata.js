0faf261d44436a274d23868603f7f057ac5c6151
Ricky <shiruiqi@126.com>
2021-01-21 00:54:24 +0800

    fix

--- src/data/tabledata.js
+++ src/data/tabledata.js
@@ -5,8 +5,8 @@ const markdown =`
 
 | Team/Project    | when to testnet   | when to mainnet  | 3rd party development supportted|  Virtual Machine/Lanuage  |
 | ----------------|:-----------------|:-----------------|:-----------------:|:----------------:|
-| [Optimism](https://optimism.io/)    | [Done](https://medium.com/ethereum-optimism/light-at-the-end-of-the-tunnel-c390a05bbcb8) but limited <br/> Public Testnet — Late February/Early March|   Soon after Public testnet|YES  | EVM |
-| [OffchainLab/Arbitrum](https://offchainlabs.com/) | [Done](https://medium.com/offchainlabs/arbitrum-rollup-testnet-full-featured-and-open-to-all-da3255b562ea) | Mar 2021 | YES|EVM |
+| [Optimism](https://optimism.io/)    | [Done](https://medium.com/ethereum-optimism/light-at-the-end-of-the-tunnel-c390a05bbcb8) but limited <br/> Public Testnet — Late February/Early March|   Soon after Public testnet|YES  | OVM(EVM) |
+| [OffchainLab/Arbitrum](https://offchainlabs.com/) | [Done](https://medium.com/offchainlabs/arbitrum-rollup-testnet-full-featured-and-open-to-all-da3255b562ea) | Mar 2021 | YES|AVM(EVM) |
 | [Matter Lab/Zksync](https://matter-labs.io/)      | Done | Done | YES, but not yet | [Zinc VM](https://zinc.zksync.io/)|
 | [Loopring](https://loopring.io/#/)   | Done | Done| NO | N/A|
 | [StarkWare/StarkEx](https://starkware.co/)  | N/A | N/A |YES|[Cario](https://starkware.co/cairo/)|
